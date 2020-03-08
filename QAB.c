8fa850f1ad1190d242d222d60a028a7875a64295
qamarali9 <qamarali9@gmail.com>
2020-03-08 10:55:00 +0530

    Add files via upload

---
+++ QAB.c
@@ -0,0 +1,5 @@
+#include<stdio.h>
+
+int main(){
+	printf("    QQQQQQ          AA         BBBBBBB\n  QQ      QQ      AA  AA       BB    BB\n  QQ      QQ     AA    AA      BB    BB\n  QQ      QQ    AA      AA     BBBBBBB\n  QQ      QQ   AAAAAAAAAAAA    BB    BB\n    QQQQQQ    AA          AA   BB    BB\n         QQ  AA            AA  BBBBBBB\n");
+}
---
+++ QAB.py
@@ -0,0 +1,9 @@
+print("""
+    QQQQQQ          AA         BBBBBBB
+  QQ      QQ      AA  AA       BB    BB
+  QQ      QQ     AA    AA      BB    BB
+  QQ      QQ    AA      AA     BBBBBBB
+  QQ      QQ   AAAAAAAAAAAA    BB    BB
+    QQQQQQ    AA          AA   BB    BB
+         QQ  AA            AA  BBBBBBB
+""")
